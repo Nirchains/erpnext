bea4e0286b7512bcbf2f691b3c50c1c98c3c67a2
Nirchains <nirchains@gmail.com>
2020-02-13 08:14:53 +0100

    bug con el idioma de un patch

--- erpnext/patches/v11_0/set_default_email_template_in_hr.py
+++ erpnext/patches/v11_0/set_default_email_template_in_hr.py
@@ -5,6 +5,6 @@ from frappe import _
 def execute():
 	#PFG bug con el idioma
 	hr_settings = frappe.get_single("HR Settings")
-	hr_settings.leave_approval_notification_template = _("Leave Approval Notification")
-	hr_settings.leave_status_notification_template = _('Leave Status Notification')
+	hr_settings.leave_approval_notification_template = "Notificación de Autorización de Vacaciones"#_("Leave Approval Notification")
+	hr_settings.leave_status_notification_template = "Estado de Notificación de Vacaciones"#_('Leave Status Notification')
 	hr_settings.save()
